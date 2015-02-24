927f98043c22911fbdc00d4370a9ab2c11400e6b
Charles Pletcher <cpletcher2@gmail.com>
2015-02-23 17:51:59 -0800

    Remove errant console.log

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,5 +5,3 @@ exports.entry.unshift('webpack/hot/dev-server');
 exports.module.loaders[0].loaders = ['babel', 'react-hot', 'jsx?harmony'];
 exports.output = Object.create(exports.output);
 exports.output.filename = exports.output.filename.replace(/\.js$/, ".dev.js");
-
-console.log(exports);
