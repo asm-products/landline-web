6ff3c2125cb3f8b5e2931bc1812c6c6ff5587b1a
lachlanjc <lachlan.campbell@icloud.com>
2015-02-24 19:39:40 -0500

    Styling improvements

--- src/components/chat/chat_input.jsx
+++ src/components/chat/chat_input.jsx
@@ -21,15 +21,19 @@ const ChatInput = React.createClass({
       user
     } = this.state;
 
+    let style = {
+      boxShadow: '-4px 0 15px rgba(0,0,0,.15)'
+    }
+
     if (user.get('username')) {
-      return (
+      return (<div className="full-width shadow px3 py1" style={style}>
         <input type="text"
             className="full-width field-light mb0"
             onKeyPress={this.submitMessage}
             onChange={this.updateBody}
             value={body}
-            placeholder="Enter your message" />
-      );
+            placeholder="What do you want to say?" />
+      </div>);
     }
   },
 
