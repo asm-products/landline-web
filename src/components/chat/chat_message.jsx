563a20c25375d6d87e9d700e75e4a46899122bc1
Charles Pletcher <cpletcher2@gmail.com>
2015-03-02 21:48:39 -0800

    Render avatars
    That took more styling than expected...

--- src/components/chat/chat_message.jsx
+++ src/components/chat/chat_message.jsx
@@ -14,11 +14,23 @@ const ChatMessage = React.createClass({
 
   render() {
     let message = this.props.message;
+    let style = {
+      avatar: {
+        borderRadius: '50%',
+        maxHeight: 30,
+        maxWidth: 30
+      }
+    };
 
     return (
-      <div className="mb1">
-        <span className="bold block">{message.Username}</span>
-        <span className="block">{message.Body}</span>
+      <div className="mb1 clearfix">
+        <div className="col col-1">
+          <img className="left" src={message.avatar_url} width={24} style={style.avatar} />
+        </div>
+        <div className="col col-11 mxn1">
+          <span className="bold block" style={style.username}>{message.Username}</span>
+          <span className="block">{message.Body}</span>
+        </div>
       </div>
     );
   }
