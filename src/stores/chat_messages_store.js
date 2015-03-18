834fed054820cda56e96a857e47e16cc43d971fc
Frederik Creemers <frederikcreemers@gmail.com>
2015-03-18 20:42:56 +0100

    Transform timestamps on messages to Date objects.

--- src/stores/chat_messages_store.js
+++ src/stores/chat_messages_store.js
@@ -8,15 +8,22 @@ const Store = require('./store');
 let unconfirmedMessages = List();
 let messages = Map();
 
+//transform a message before saving it.
+let transformMessage = function(msg){
+  msg.created_at = new Date(msg.created_at);
+  msg.last_online_at = new Date(msg.last_online_at);
+  return msg
+}
+
 class ChatMessagesStore extends Store {
   constructor() {
     this.dispatchToken = Dispatcher.register((action) => {
       switch (action.actionType) {
         case ActionTypes.CHAT_MESSAGES_RECEIVED:
-          messages = messages.set(action.channel, List(action.messages) );
+          messages = messages.set(action.channel, List(action.messages).map(transformMessage);
           break;
         case ActionTypes.CHAT_SERVER_MESSAGE_RECEIVED:
-          messages = messages.set(action.channel, messages.get(action.channel, List()).push(action.message));
+          messages = messages.set(action.channel, messages.get(action.channel, List()).push(transformMessage(action.message)));
           break;
         case ActionTypes.CHAT_MESSAGE_SUBMITTED:
           unconfirmedMessages = unconfirmedMessages.push(action.message);
