6af3e80dd4719be0253203b8669efbd78a30db04
Charles Pletcher <cpletcher2@gmail.com>
2015-03-18 10:54:47 -0700

    Add dist back to the repo
    The build is failing despite having the appropriate loaders; I'll need
    to investigate more

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,8 +1,6 @@
 var webpack = require('webpack');
 
 module.exports = exports = Object.create(require('./webpack.config'));
-// exports.entry.unshift('webpack/hot/dev-server');
-// exports.module.loaders[0].loaders = ['babel', 'react-hot', 'jsx?harmony'];
 exports.plugins[1] = new webpack.DefinePlugin({
   __API_URL__: JSON.stringify('https://landline-dev.herokuapp.com'),
   __PROD__: false,
